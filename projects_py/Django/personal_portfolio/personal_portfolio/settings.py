604e94643778a010ef405a2fae6e003b87d9e079
ZamaaN <zamaan.md19@gmail.com>
2021-04-22 01:01:30 +0600

    Update settings.py

--- projects_py/Django/personal_portfolio/personal_portfolio/settings.py
+++ projects_py/Django/personal_portfolio/personal_portfolio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''' REMOVED FOR SECURITY REASON. YOU WILL GET YOUR OWN WHILE CRATING APP. '''
+SECRET_KEY = ''' REMOVED FOR SECURITY REASON. YOU WILL GET YOUR OWN WHILE CREATING APP. '''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
